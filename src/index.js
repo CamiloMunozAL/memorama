a87dd10cee4b6f24f7e53ee4b3aa77843e09d0bd
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-06-26 00:58:04 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./css/card_comparator_logic.css";
+import "./css/randomness.css";
+import "./css/start.css";
+import "./css/animation.css";
+import "./css/notifications.css";
+import "./css/responsive.css";
+import "./css/header.css";
